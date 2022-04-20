bf5597e10b7da6f9fb6172300bb8c2f35b116b57
WILLIAN CARDOSO DE ARAUJO JUNIOR <willian.cajunior@senacsp.edu.br>
2022-04-20 13:59:58 -0300

    first commit

---
+++ ExerciciosTI14t/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosTI14t
+{
+    class controlExercícios
+    {
+        class ControlCalculadora
+        {
+
+
+                
+                
+            private double num1;
+            public void Coletar()
+        {
+            Console.WriteLine("Informe o primeiro Número: ");
+             = Convert.ToDouble(Console.ReadLine());
+   
+        }// fim do coletar
+    }
+}
---
+++ ExerciciosTI14t/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosTI14t
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
